74c38d68cbe6363d12679fb2315843bde4de1f74
emmanuel <emmanueloringe@gmail.com>
2025-10-04 12:28:20 +0300

    updated the Landing Page

--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -7,7 +7,7 @@ export default function WelcomeScreen() {
 
   return (
     <ImageBackground 
-      source={{ uri: 'https://images.unsplash.com/photo-1559757148-5c350d0d3c56?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80' }}
+      source={require('../../assets/images/LandingPage3.jpg')}
       style={styles.container}
       resizeMode="cover"
     >
